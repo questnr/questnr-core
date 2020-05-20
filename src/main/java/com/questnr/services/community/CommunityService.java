530333db2fff73478c9946f2af852aede7f3477a
brijeshlakkad <brijeshlakkad22@gmail.com>
2020-05-21 01:36:01 +0530

    Community meta tags added

--- src/main/java/com/questnr/services/community/CommunityService.java
+++ src/main/java/com/questnr/services/community/CommunityService.java
@@ -10,6 +10,7 @@ import com.questnr.model.repositories.CommunityRepository;
 import com.questnr.services.AmazonS3Client;
 import com.questnr.services.CommonService;
 import com.questnr.services.CustomPageService;
+import com.questnr.services.SharableLinkService;
 import com.questnr.services.user.UserCommonService;
 import com.questnr.util.SecureRandomService;
 import org.slf4j.Logger;
@@ -55,6 +56,12 @@ public class CommunityService {
     @Value("${questnr.domain}")
     String QUEST_NR_DOMAIN;
 
+    @Autowired
+    SharableLinkService sharableLinkService;
+
+    @Value("${facebook.appid}")
+    private String fbAppId;
+
     final private String COMMUNITY_PATH = "community";
 
     private String createCommunitySlug(Community community) {
@@ -67,7 +74,7 @@ public class CommunityService {
 
     private String getCommunityTags(Community community) {
         List<String> descChunks = Arrays.asList(community.getDescription().toLowerCase().split("\\s"));
-        return CommonService.removeSpecialCharacters(String.join(" ", descChunks.subList(0, descChunks.size())));
+        return CommonService.removeSpecialCharacters(String.join(", ", descChunks.subList(0, descChunks.size())));
     }
 
     public Community getCommunityByCommunityName(String communityName) {
@@ -78,36 +85,109 @@ public class CommunityService {
         throw new ResourceNotFoundException("Community not found!");
     }
 
-    private CommunityMetaInformation getCommunityDescMetaInformation(CommunityDTO communityDTO) {
+    private CommunityMetaInformation getCommunityMetaInformation(String attrType, String type, String content){
         MetaInformation metaInfo = new MetaInformation();
-        metaInfo.setAttributeType("name");
-        metaInfo.setType("description");
-        metaInfo.setContent(CommonService.removeSpecialCharacters(communityDTO.getDescription()));
-        CommunityMetaInformation communityMeta = new CommunityMetaInformation();
-        communityMeta.setMetaInformation(metaInfo);
-        return communityMeta;
+        metaInfo.setAttributeType(attrType);
+        metaInfo.setType(type);
+        metaInfo.setContent(content);
+        CommunityMetaInformation communityMetaInformation = new CommunityMetaInformation();
+        communityMetaInformation.setMetaInformation(metaInfo);
+        return communityMetaInformation;
+    }
+
+    private List<CommunityMetaInformation> getCommunityMetaInformationList(CommunityDTO communityDTO) {
+        List<CommunityMetaInformation> communityMetaInformationList = new ArrayList<>();
+
+        communityMetaInformationList.add(this.getCommunityMetaInformation(
+                "name",
+                "description",
+                CommonService.removeSpecialCharacters(communityDTO.getDescription())
+        ));
+
+        communityMetaInformationList.add(this.getCommunityMetaInformation(
+                "name",
+                "author",
+                communityDTO.getOwnerUserDTO().getFirstName()+ " "+ communityDTO.getOwnerUserDTO().getFirstName()
+        ));
+
+        communityMetaInformationList.add(this.getCommunityMetaInformation(
+                "name",
+                "robots",
+                "index, follow, max-image-preview:standard"
+        ));
+
+        communityMetaInformationList.add(this.getCommunityMetaInformation(
+                "name",
+                "googlebot",
+                "index, follow, max-image-preview:standard"
+        ));
+
+        communityMetaInformationList.add(this.getCommunityMetaInformation(
+                "property",
+                "og:url",
+                sharableLinkService.getCommunitySharableLink(communityDTO.getSlug()).getClickAction()
+        ));
+
+        communityMetaInformationList.add(this.getCommunityMetaInformation(
+                "property",
+                "og:title",
+                communityDTO.getCommunityName()
+        ));
+
+        communityMetaInformationList.add(this.getCommunityMetaInformation(
+                "property",
+                "og:image",
+                communityDTO.getAvatarDTO().getAvatarLink()
+        ));
+
+        communityMetaInformationList.add(this.getCommunityMetaInformation(
+                "property",
+                "og:type",
+                "website"
+        ));
+
+        communityMetaInformationList.add(this.getCommunityMetaInformation(
+                "property",
+                "fb:app_id",
+                fbAppId
+        ));
+
+        communityMetaInformationList.add(this.getCommunityMetaInformation(
+                "property",
+                "twitter:title",
+                communityDTO.getCommunityName()
+        ));
+
+        communityMetaInformationList.add(this.getCommunityMetaInformation(
+                "property",
+                "twitter:description",
+                communityDTO.getDescription()
+        ));
+
+        communityMetaInformationList.add(this.getCommunityMetaInformation(
+                "property",
+                "twitter:url",
+                sharableLinkService.getCommunitySharableLink(communityDTO.getSlug()).getClickAction()
+        ));
+
+        communityMetaInformationList.add(this.getCommunityMetaInformation(
+                "property",
+                "twitter:image",
+                communityDTO.getAvatarDTO().getAvatarLink()
+        ));
+
+        communityMetaInformationList.add(this.getCommunityMetaInformation(
+                "property",
+                "twitter:image",
+                communityDTO.getAvatarDTO().getAvatarLink()
+        ));
+
+        return communityMetaInformationList;
     }
 
     public CommunityDTO setCommunityMetaInformation(CommunityDTO communityDTO) {
         if (communityDTO != null) {
-            List<CommunityMetaInformation> metaList = new LinkedList<>();
-            if (communityDTO.getMetaList() == null || communityDTO.getMetaList().size() == 0) {
-                metaList.add(this.getCommunityDescMetaInformation(communityDTO));
-            } else {
-                boolean foundDesc = false;
-                for (CommunityMetaInformation meta : communityDTO.getMetaList()) {
-                    if (meta != null && meta.getMetaInformation() != null) {
-                        if (meta.getMetaInformation().getType().equals("description")) {
-                            foundDesc = true;
-                            break;
-                        }
-                    }
-                }
-                if (!foundDesc) {
-                    metaList.add(this.getCommunityDescMetaInformation(communityDTO));
-                }
-            }
-            communityDTO.getMetaList().addAll(metaList);
+            communityDTO.getMetaList().addAll(this.getCommunityMetaInformationList(communityDTO));
         }
         return communityDTO;
     }
