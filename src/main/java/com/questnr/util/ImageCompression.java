d9f383e2854895368027ea55d3939d9b063e2932
Brijesh Lakkad <brijeshlakkad22@gmail.com>
2020-05-26 01:49:05 +0530

    Image compression Quality set to 0.1f

--- src/main/java/com/questnr/util/ImageCompression.java
+++ src/main/java/com/questnr/util/ImageCompression.java
@@ -41,7 +41,7 @@ public class ImageCompression {
 
         if (param.canWriteCompressed()) {
             param.setCompressionMode(ImageWriteParam.MODE_EXPLICIT);
-            param.setCompressionQuality(0.5f);
+            param.setCompressionQuality(0.1f);
         }
 
         writer.write(null, new IIOImage(image, null, null), param);
