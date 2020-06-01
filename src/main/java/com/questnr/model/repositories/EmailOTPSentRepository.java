6d7f71434397ae383397091c6a2479dc720a14c1
Brijesh Lakkad <brijeshlakkad22@gmail.com>
2020-06-02 02:50:16 +0530

    MAX_NUMBER_OF_EMAIL_PER_DAY added to set limit to the number of email being sent

--- src/main/java/com/questnr/model/repositories/EmailOTPSentRepository.java
+++ src/main/java/com/questnr/model/repositories/EmailOTPSentRepository.java
@@ -3,9 +3,18 @@ package com.questnr.model.repositories;
 import com.questnr.model.entities.EmailOTPSent;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Date;
+import java.util.List;
+
 public interface EmailOTPSentRepository extends JpaRepository<EmailOTPSent, Long> {
 
   EmailOTPSent findByEmail(String email);
+
+  List<EmailOTPSent> findByEmailOrderByCreatedAtDesc(String email);
+
+  List<EmailOTPSent> findAllByEmailAndCreatedAtBetween(String email, Date startingDate, Date endingDate);
+
+  int countAllByEmailAndCreatedAtBetween(String email, Date startingDate, Date endingDate);
 
   boolean existsByEmailAndOtp(String email, String otp);
 }
--- src/main/java/com/questnr/services/OTPService.java
+++ src/main/java/com/questnr/services/OTPService.java
@@ -12,6 +12,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
+import java.util.List;
+
 @Service
 public class OTPService {
     private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
@@ -28,47 +31,58 @@ public class OTPService {
     @Autowired
     CommonService commonService;
 
+    final int MAX_NUMBER_OF_EMAIL_PER_DAY = 4;
+
+    final int ONE_DAY = 86400 * 1000;
+
+    private boolean hasCrossedMaxLimitRequest(UserEmailRequest userEmailRequest) throws InvalidRequestException{
+        Date endingDate = new Date();
+        Date startingDate = new Date(endingDate.getTime() - ONE_DAY);
+        int numberOfHasBeenSentToday = emailOTPSentRepository.countAllByEmailAndCreatedAtBetween(userEmailRequest.getEmail(), startingDate, endingDate);
+        if(numberOfHasBeenSentToday > MAX_NUMBER_OF_EMAIL_PER_DAY) {
+            throw new InvalidRequestException("You have crossed maximum limit per day!");
+        }
+        return false;
+    }
+
     public void sendOTP(UserEmailRequest userEmailRequest){
-        try{
-            EmailOTPSent emailOTPSent = emailOTPSentRepository.findByEmail(userEmailRequest.getEmail());
-            if(emailOTPSent != null){
-                emailOTPSent.updateMetadata();
-            }else{
-                emailOTPSent = new EmailOTPSent();
-                emailOTPSent.setEmail(userEmailRequest.getEmail());
-                emailOTPSent.addMetadata();
-            }
+        if(!this.hasCrossedMaxLimitRequest(userEmailRequest)) {
+            EmailOTPSent emailOTPSent = new EmailOTPSent();
+            emailOTPSent.setEmail(userEmailRequest.getEmail());
+            emailOTPSent.addMetadata();
             emailOTPSent.setOtp(commonService.getRandomNumber());
             EmailOTPSent savedEmailOTPSent = emailOTPSentRepository.save(emailOTPSent);
             emailService.sendOTPEmail(savedEmailOTPSent);
-        }catch (Exception e){
-            throw new InvalidRequestException("Please, try again!");
         }
     }
 
     public void resendOTP(UserEmailRequest userEmailRequest){
-        try{
-            EmailOTPSent emailOTPSent = emailOTPSentRepository.findByEmail(userEmailRequest.getEmail());
-            if(emailOTPSent == null){
+        if(!this.hasCrossedMaxLimitRequest(userEmailRequest)) {
+            List<EmailOTPSent> emailOTPSentList = emailOTPSentRepository.findByEmailOrderByCreatedAtDesc(userEmailRequest.getEmail());
+            if (emailOTPSentList.size() == 0) {
+                EmailOTPSent emailOTPSent = new EmailOTPSent();
                 emailOTPSent.addMetadata();
+                emailOTPSent.setEmail(userEmailRequest.getEmail());
                 emailOTPSent.setOtp(commonService.getRandomNumber());
                 EmailOTPSent savedEmailOTPSent = emailOTPSentRepository.save(emailOTPSent);
                 emailService.sendOTPEmail(savedEmailOTPSent);
+            } else {
+                EmailOTPSent reEmailOTPSent = new EmailOTPSent();
+                reEmailOTPSent.addMetadata();
+                reEmailOTPSent.setEmail(userEmailRequest.getEmail());
+                reEmailOTPSent.setOtp(emailOTPSentList.get(0).getOtp());
+                EmailOTPSent savedEmailOTPSent = emailOTPSentRepository.save(reEmailOTPSent);
+                emailService.sendOTPEmail(savedEmailOTPSent);
             }
-            else{
-                emailService.sendOTPEmail(emailOTPSent);
-            }
-        }catch (Exception e){
-            throw new InvalidRequestException("Please, try again!");
         }
     }
 
     public BooleanResponse verifyOTP(OTPVerificationRequest otpVerificationRequest){
         BooleanResponse booleanResponse = new BooleanResponse();
         try{
-            EmailOTPSent emailOTPSent = emailOTPSentRepository.findByEmail(otpVerificationRequest.getEmail());
-            if(emailOTPSent != null){
-                if(otpVerificationRequest.getOtp().equals(emailOTPSent.getOtp())){
+            List<EmailOTPSent> emailOTPSentList = emailOTPSentRepository.findByEmailOrderByCreatedAtDesc(otpVerificationRequest.getEmail());
+            if(emailOTPSentList.size() != 0){
+                if(otpVerificationRequest.getOtp().equals(emailOTPSentList.get(0).getOtp())){
                     booleanResponse.setStatus(true);
                 }
             }
