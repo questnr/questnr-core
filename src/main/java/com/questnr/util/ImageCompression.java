23efc4960768114b82be50cc18e79ade48abd965
Brijesh Lakkad <brijeshlakkad22@gmail.com>
2020-06-08 18:08:13 +0530

    Compression with 0.6 quality has been found good

--- src/main/java/com/questnr/util/ImageCompression.java
+++ src/main/java/com/questnr/util/ImageCompression.java
@@ -55,7 +55,7 @@ public class ImageCompression {
 
         if (param.canWriteCompressed()) {
             param.setCompressionMode(ImageWriteParam.MODE_EXPLICIT);
-            param.setCompressionQuality(0.1f);
+            param.setCompressionQuality(0.6f);
         }
 
         writer.write(null, new IIOImage(image, null, null), param);
