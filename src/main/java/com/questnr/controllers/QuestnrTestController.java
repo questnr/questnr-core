ca7cdf732ae8bf70c6466fa93aaab88f730cb662
Brijesh Lakkad <brijeshlakkad22@gmail.com>
2020-07-26 18:17:30 +0530

    SET BLOG TITLE SCRIPT

--- src/main/java/com/questnr/controllers/QuestnrTestController.java
+++ src/main/java/com/questnr/controllers/QuestnrTestController.java
@@ -1,15 +1,11 @@
 package com.questnr.controllers;
 
-import com.questnr.common.enums.PostActionPrivacy;
 import com.questnr.services.AmazonS3Client;
 import com.questnr.services.CommonService;
 import com.questnr.services.QuestnrTestService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.io.IOException;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/api/v1/admin")
@@ -24,18 +20,24 @@ public class QuestnrTestController {
     @Autowired
     private CommonService commonService;
 
-    @RequestMapping(value = "/video-compression/manipuate/{quality}", method = RequestMethod.POST, consumes = {MediaType.MULTIPART_FORM_DATA_VALUE}, produces = {MediaType.APPLICATION_JSON_VALUE})
-    public String manipulateVideo(@RequestPart(value = "file") MultipartFile file, @PathVariable int quality) throws IOException, InterruptedException {
-        return questnrTestService.manipulateVideo(file, quality);
-    }
+//    @RequestMapping(value = "/video-compression/manipuate/{quality}", method = RequestMethod.POST, consumes = {MediaType.MULTIPART_FORM_DATA_VALUE}, produces = {MediaType.APPLICATION_JSON_VALUE})
+//    public String manipulateVideo(@RequestPart(value = "file") MultipartFile file, @PathVariable int quality) throws IOException, InterruptedException {
+//        return questnrTestService.manipulateVideo(file, quality);
+//    }
+//
+//    @RequestMapping(value = "/upload/private", method = RequestMethod.POST, consumes = {MediaType.MULTIPART_FORM_DATA_VALUE}, produces = {MediaType.APPLICATION_JSON_VALUE})
+//    public void uploadFilePrivate(@RequestPart(value = "file") MultipartFile file) throws Exception{
+//        this.amazonS3Client.uploadFile(commonService.convertMultiPartToFile(file), PostActionPrivacy.private_post);
+//    }
+//
+//    @RequestMapping(value = "/upload/public", method = RequestMethod.POST, consumes = {MediaType.MULTIPART_FORM_DATA_VALUE}, produces = {MediaType.APPLICATION_JSON_VALUE})
+//    public void uploadFilePublic(@RequestPart(value = "file") MultipartFile file) throws Exception{
+//        this.amazonS3Client.uploadFile(commonService.convertMultiPartToFile(file), PostActionPrivacy.public_post);
+//    }
 
-    @RequestMapping(value = "/upload/private", method = RequestMethod.POST, consumes = {MediaType.MULTIPART_FORM_DATA_VALUE}, produces = {MediaType.APPLICATION_JSON_VALUE})
-    public void uploadFilePrivate(@RequestPart(value = "file") MultipartFile file) throws Exception{
-        this.amazonS3Client.uploadFile(commonService.convertMultiPartToFile(file), PostActionPrivacy.private_post);
-    }
-
-    @RequestMapping(value = "/upload/public", method = RequestMethod.POST, consumes = {MediaType.MULTIPART_FORM_DATA_VALUE}, produces = {MediaType.APPLICATION_JSON_VALUE})
-    public void uploadFilePublic(@RequestPart(value = "file") MultipartFile file) throws Exception{
-        this.amazonS3Client.uploadFile(commonService.convertMultiPartToFile(file), PostActionPrivacy.public_post);
-    }
+//    // Set blog title if user has blog title as null
+//    @RequestMapping(value = "/blog/title", method = RequestMethod.POST)
+//    public Map<String, Integer> makeBlogTitle(){
+//        return questnrTestService.makeBlogTitle();
+//    }
 }
--- src/main/java/com/questnr/services/QuestnrTestService.java
+++ src/main/java/com/questnr/services/QuestnrTestService.java
@@ -1,5 +1,8 @@
 package com.questnr.services;
 
+import com.questnr.common.enums.PostEditorType;
+import com.questnr.model.entities.PostAction;
+import com.questnr.model.repositories.PostActionRepository;
 import com.questnr.services.user.UserCommonService;
 import com.questnr.util.VideoCompression;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +11,9 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Service
 public class QuestnrTestService {
@@ -18,14 +24,41 @@ public class QuestnrTestService {
     @Autowired
     private CommonService commonService;
 
+    @Autowired
+    PostActionRepository postActionRepository;
+
+    @Autowired
+    PostActionService postActionService;
+
     public String manipulateVideo(MultipartFile multipartFile, int quality) throws IOException, InterruptedException {
         File source = commonService.convertMultiPartToFile(multipartFile);
-        File target = new File("out_"+source.getName());
-        VideoCompression videoCompression = new VideoCompression(source,target);
+        File target = new File("out_" + source.getName());
+        VideoCompression videoCompression = new VideoCompression(source, target);
         videoCompression.setQuality(quality);
         Thread videoCompressionThread = new Thread(videoCompression, "VideoCompression-1");
         videoCompressionThread.start();
         videoCompressionThread.join();
         return "Successful";
+    }
+
+    public Map<String, Integer> makeBlogTitle() {
+        List<PostAction> postActionList = postActionRepository.findAll();
+        Map<String, Integer> response = new HashMap<>();
+        int count = 0;
+        int blogCount = 0;
+        for (PostAction postAction : postActionList) {
+            if (postAction.getPostEditorType() == PostEditorType.blog) {
+                if (CommonService.isNull(postAction.getBlogTitle())) {
+                    postAction.setTags(postActionService.getPostActionTitleTag(postAction.getText()));
+                    postAction.setBlogTitle(postAction.getTags().substring(0, Math.min(postAction.getTags().length(), 20))+ "...");
+                    postActionRepository.save(postAction);
+                    count++;
+                }
+                blogCount++;
+            }
+        }
+        response.put("affected", count);
+        response.put("totalBlogs", blogCount);
+        return response;
     }
 }
