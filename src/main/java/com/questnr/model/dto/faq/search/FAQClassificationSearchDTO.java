66221fd3ff85504563efe6460328cbda3bd6ac4b
Brijesh Lakkad <brijeshlakkad22@gmail.com>
2020-09-11 00:10:51 +0530

    publicId will be sent when faq items will be searched to redirect it to its class

--- src/main/java/com/questnr/model/dto/faq/search/FAQClassificationSearchDTO.java
+++ src/main/java/com/questnr/model/dto/faq/search/FAQClassificationSearchDTO.java
@@ -3,9 +3,19 @@ package com.questnr.model.dto.faq.search;
 import com.questnr.services.CommonService;
 
 public class FAQClassificationSearchDTO {
+    private Long publicId;
+
     private String category;
 
     private String description;
+
+    public Long getPublicId() {
+        return publicId;
+    }
+
+    public void setPublicId(Long publicId) {
+        this.publicId = publicId;
+    }
 
     public String getCategory() {
         return CommonService.titleCase(this.category);
--- src/main/java/com/questnr/model/mapper/FAQClassificationMapper.java
+++ src/main/java/com/questnr/model/mapper/FAQClassificationMapper.java
@@ -4,11 +4,13 @@ import com.questnr.model.dto.faq.FAQClassificationDTO;
 import com.questnr.model.dto.faq.search.FAQClassificationSearchDTO;
 import com.questnr.model.entities.FAQClassification;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
 
 @Mapper(uses = {UserMapper.class}, unmappedTargetPolicy = ReportingPolicy.IGNORE, componentModel = "spring")
 public abstract class FAQClassificationMapper {
     public abstract FAQClassificationDTO toDTO(final FAQClassification faqClassification);
 
-    public abstract FAQClassificationSearchDTO toClassificationDTO(final FAQClassification faqClassification);
+    @Mapping(source = "faqClassificationId", target = "publicId")
+    public abstract FAQClassificationSearchDTO toSearchDTO(final FAQClassification faqClassification);
 }
