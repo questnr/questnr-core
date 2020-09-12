835a94ad1d87a07af883c8ea9bd8eba6c8ad379c
Brijesh Lakkad <brijeshlakkad22@gmail.com>
2020-09-12 13:10:05 +0530

    Added questnr.com as only ALLOWED origin

--- src/main/java/com/questnr/config/CORSConfig.java
+++ src/main/java/com/questnr/config/CORSConfig.java
@@ -2,22 +2,30 @@ package com.questnr.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
+import java.util.List;
 
 @Configuration
+@Order(Ordered.HIGHEST_PRECEDENCE)
 public class CORSConfig {
     @Bean
     public CorsFilter corsFilter() {
+        List<String> allowedOrigins = new ArrayList<>();
+        allowedOrigins.add("https://questnr.com");
+        allowedOrigins.add("https://www.questnr.com");
+
         final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         final CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
         // @Todo Don't do this in production, use a proper list  of allowed origins
-        config.setAllowedOrigins(Collections.singletonList("*"));
+        config.setAllowedOrigins(allowedOrigins);
         config.setAllowedHeaders(Arrays.asList("*"));
 //        config.setAllowedHeaders(Arrays.asList("Origin", "Content-Type", "Accept"));
         config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "OPTIONS", "DELETE", "PATCH"));
