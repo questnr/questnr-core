81a052c704b8eb49ce49f2f62d6d038a7d71339d
Brijesh Lakkad <brijeshlakkad22@gmail.com>
2020-09-12 18:33:25 +0530

    isProd variable used to solve allowedOrigin set to * when the environment is development

--- src/main/java/com/questnr/config/CORSConfig.java
+++ src/main/java/com/questnr/config/CORSConfig.java
@@ -1,5 +1,6 @@
 package com.questnr.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
@@ -10,11 +11,15 @@ import org.springframework.web.filter.CorsFilter;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 @Configuration
 @Order(Ordered.HIGHEST_PRECEDENCE)
 public class CORSConfig {
+    @Value("${app.is-prod}")
+    boolean isProd;
+
     @Bean
     public CorsFilter corsFilter() {
         List<String> allowedOrigins = new ArrayList<>();
@@ -25,7 +30,7 @@ public class CORSConfig {
         final CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
         // @Todo Don't do this in production, use a proper list  of allowed origins
-        config.setAllowedOrigins(allowedOrigins);
+        config.setAllowedOrigins(isProd ? allowedOrigins : Collections.singletonList("*"));
         config.setAllowedHeaders(Arrays.asList("*"));
 //        config.setAllowedHeaders(Arrays.asList("Origin", "Content-Type", "Accept"));
         config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "OPTIONS", "DELETE", "PATCH"));
