af8fda7dc1e0236fd793572765b458ad32ea4702
Brijeshlakkad <brijeshlakkad22@gmail.com>
2020-10-29 22:09:51 +0530

    User does not have any registered email id, error will be thrown if the signing up user with Social Login have not registered their email with the account their are linking

--- src/main/java/com/questnr/domain/SocialUserDetails.java
+++ src/main/java/com/questnr/domain/SocialUserDetails.java
@@ -3,8 +3,10 @@ package com.questnr.domain;
 import com.questnr.common.enums.LoginType;
 import com.questnr.domain.facebook.FBUserDetails;
 import com.questnr.domain.google.GoogleUserDetails;
+import com.questnr.exceptions.InvalidRequestException;
 import com.questnr.model.entities.User;
 import com.questnr.services.BaseService;
+import com.questnr.services.CommonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -37,56 +39,66 @@ public class SocialUserDetails {
     }
 
     public User getUser(GoogleUserDetails googleUserDetails) {
-        User user = new User();
-        user.setSocialId(googleUserDetails.getSub());
-        user.setEmailId(googleUserDetails.getEmail());
+        if (googleUserDetails != null
+                && googleUserDetails.getEmail() != null
+                && !CommonService.isNull(googleUserDetails.getEmail())) {
+            User user = new User();
+            user.setSocialId(googleUserDetails.getSub());
+            user.setEmailId(googleUserDetails.getEmail());
 //        user.setFullName(googleUserDetails.getName());
 
-        // Create username
-        if (googleUserDetails.getName() != null)
-            user.setUsername(baseService.createUsername(googleUserDetails.getName()));
-        else if (googleUserDetails.getGiven_name() != null || googleUserDetails.getFamily_name() != null) {
-            if (googleUserDetails.getGiven_name() != null && googleUserDetails.getFamily_name() != null) {
-                user.setUsername(baseService.createUsername(googleUserDetails.getGiven_name() + googleUserDetails.getGiven_name()));
-            } else if (googleUserDetails.getGiven_name() != null && googleUserDetails.getFamily_name() == null) {
-                user.setUsername(baseService.createUsername(googleUserDetails.getGiven_name()));
-            } else if (googleUserDetails.getGiven_name() == null && googleUserDetails.getFamily_name() != null) {
-                user.setUsername(baseService.createUsername(googleUserDetails.getFamily_name()));
-            }
-        } else
-            user.setUsername(baseService.createUsername(this.getUsernameFromEmail(googleUserDetails/**/.getEmail())));
+            // Create username
+            if (googleUserDetails.getName() != null)
+                user.setUsername(baseService.createUsername(googleUserDetails.getName()));
+            else if (googleUserDetails.getGiven_name() != null || googleUserDetails.getFamily_name() != null) {
+                if (googleUserDetails.getGiven_name() != null && googleUserDetails.getFamily_name() != null) {
+                    user.setUsername(baseService.createUsername(googleUserDetails.getGiven_name() + googleUserDetails.getGiven_name()));
+                } else if (googleUserDetails.getGiven_name() != null && googleUserDetails.getFamily_name() == null) {
+                    user.setUsername(baseService.createUsername(googleUserDetails.getGiven_name()));
+                } else if (googleUserDetails.getGiven_name() == null && googleUserDetails.getFamily_name() != null) {
+                    user.setUsername(baseService.createUsername(googleUserDetails.getFamily_name()));
+                }
+            } else
+                user.setUsername(baseService.createUsername(this.getUsernameFromEmail(googleUserDetails/**/.getEmail())));
 
-        user.setFirstName(this.processName(googleUserDetails.getGiven_name()));
-        user.setLastName(this.processName(googleUserDetails.getFamily_name()));
-        user.setEmailVerified(googleUserDetails.getEmail_verified());
-        user.setLoginType(LoginType.GOOGLE);
-        return user;
+            user.setFirstName(this.processName(googleUserDetails.getGiven_name()));
+            user.setLastName(this.processName(googleUserDetails.getFamily_name()));
+            user.setEmailVerified(googleUserDetails.getEmail_verified());
+            user.setLoginType(LoginType.GOOGLE);
+            return user;
+        }
+        throw new InvalidRequestException("User does not have any registered email id");
     }
 
     public User getUser(FBUserDetails fbUserDetails) {
-        User user = new User();
-        user.setSocialId(fbUserDetails.getId());
-        user.setEmailId(fbUserDetails.getEmail());
+        if (fbUserDetails != null
+                && fbUserDetails.getEmail() != null
+                && !CommonService.isNull(fbUserDetails.getEmail())) {
+            User user = new User();
+            user.setSocialId(fbUserDetails.getId());
+            user.setEmailId(fbUserDetails.getEmail());
 //        user.setFullName(fbUserDetails.getName());
 
-        // Create username
-        if (fbUserDetails.getName() != null)
-            user.setUsername(baseService.createUsername(fbUserDetails.getName()));
-        else if (fbUserDetails.getFirst_name() != null || fbUserDetails.getLast_name() != null) {
-            if (fbUserDetails.getFirst_name() != null && fbUserDetails.getLast_name() != null) {
-                user.setUsername(baseService.createUsername(fbUserDetails.getFirst_name() + fbUserDetails.getLast_name()));
-            } else if (fbUserDetails.getFirst_name() != null && fbUserDetails.getLast_name() == null) {
-                user.setUsername(baseService.createUsername(fbUserDetails.getFirst_name()));
-            } else if (fbUserDetails.getFirst_name() == null && fbUserDetails.getLast_name() != null) {
-                user.setUsername(baseService.createUsername(fbUserDetails.getLast_name()));
-            }
-        } else
-            user.setUsername(baseService.createUsername(this.getUsernameFromEmail(fbUserDetails.getEmail())));
+            // Create username
+            if (fbUserDetails.getName() != null)
+                user.setUsername(baseService.createUsername(fbUserDetails.getName()));
+            else if (fbUserDetails.getFirst_name() != null || fbUserDetails.getLast_name() != null) {
+                if (fbUserDetails.getFirst_name() != null && fbUserDetails.getLast_name() != null) {
+                    user.setUsername(baseService.createUsername(fbUserDetails.getFirst_name() + fbUserDetails.getLast_name()));
+                } else if (fbUserDetails.getFirst_name() != null && fbUserDetails.getLast_name() == null) {
+                    user.setUsername(baseService.createUsername(fbUserDetails.getFirst_name()));
+                } else if (fbUserDetails.getFirst_name() == null && fbUserDetails.getLast_name() != null) {
+                    user.setUsername(baseService.createUsername(fbUserDetails.getLast_name()));
+                }
+            } else
+                user.setUsername(baseService.createUsername(this.getUsernameFromEmail(fbUserDetails.getEmail())));
 
-        user.setFirstName(this.processName(fbUserDetails.getFirst_name()));
-        user.setLastName(this.processName(fbUserDetails.getLast_name()));
-        user.setEmailVerified(true);
-        user.setLoginType(LoginType.FACEBOOK);
-        return user;
+            user.setFirstName(this.processName(fbUserDetails.getFirst_name()));
+            user.setLastName(this.processName(fbUserDetails.getLast_name()));
+            user.setEmailVerified(true);
+            user.setLoginType(LoginType.FACEBOOK);
+            return user;
+        }
+        throw new InvalidRequestException("User does not have any registered email id");
     }
 }
